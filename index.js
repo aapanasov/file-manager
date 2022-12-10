7d9f5c59f81a821357333571bf445aea7817a389
aapanasov <aapanasov@gmail.com>
2022-12-10 11:06:56 +0300

    feat: change login way

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 import { createInterface } from 'node:readline/promises';
 import { homedir } from 'node:os';
 
-const userName = process.argv[2]?.split('=')[1] || 'Anonymous';
-let currentDir = homedir;
-
-const commands = ['cd', 'ls', 'up', '.exit'];
-
 const colors = {
   yellow: '\x1b[33m',
   green: '\x1b[32m',
   red: '\x1b[31m',
   reset: '\x1b[0m'
 };
+
+const args = process.argv[2]?.split('=');
+
+if (!(args[0] === '--username' && args[1])) {
+  console.log('Please start in following way:', colors.yellow, 'npm run start -- --username=your_username', colors.reset);
+  process.exit();
+}
+
+const userName = args[1];
+
+let currentDir = homedir;
+
+const commands = ['cd', 'ls', 'up', '.exit'];
 
 const onExit = () => {
   console.log(`\nThank you for using File Manager, ${colors.yellow}${userName}${colors.reset}, goodbye!`);
